5719e6b3fec8d4f276b0d4ec953ec190f7559d68
kaaviya98 <kaaviya@testpress.in>
2022-11-15 15:46:52 +0530

    Refactor:Use Black and flake

--- mysite/blog/tests/test_managers.py
+++ mysite/blog/tests/test_managers.py
@@ -3,9 +3,6 @@ from blog.models import Post
 from blog.tests.test_modelmixintestcase import ModelMixinTestCase
 
 
-class TestPublishedManager(ModelMixinTestCase,TestCase):
-
-   def test_published_manager_should_not_have_draft_post(self):
-        self.assertTrue(
-	            self.draft_post not in Post.published.all()
-	        )
+class TestPublishedManager(ModelMixinTestCase, TestCase):
+    def test_published_manager_should_not_have_draft_post(self):
+        self.assertTrue(self.draft_post not in Post.published.all())
--- mysite/blog/tests/test_modelmixintestcase.py
+++ mysite/blog/tests/test_modelmixintestcase.py
@@ -5,20 +5,20 @@ from django.contrib.auth.models import User
 
 class ModelMixinTestCase(TestCase):
     def setUp(self):
-        self.user=User.objects.create_user(
-            username= "kaaviya",
+        self.user = User.objects.create_user(
+            username="kaaviya",
             password="123",
-            )   
-
-        self.draft_post= Post.objects.create(
-            title='description',
-            author=self.user,
-            body='hi i am kaaviya',
-            status='draft'
         )
-        self.published_post= Post.objects.create(
-            title='testing title',
+
+        self.draft_post = Post.objects.create(
+            title="description",
             author=self.user,
-            body='hi i am Madhu',
-            status='published'
+            body="hi i am kaaviya",
+            status="draft",
+        )
+        self.published_post = Post.objects.create(
+            title="testing title",
+            author=self.user,
+            body="hi i am Madhu",
+            status="published",
         )
