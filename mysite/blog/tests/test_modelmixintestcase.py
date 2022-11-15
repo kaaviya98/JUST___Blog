4fe599df21902144328e59e534bb5ebcd6d5248e
kaaviya98 <kaaviya@testpress.in>
2022-11-15 11:29:40 +0530

    Refactor : Testcases

--- mysite/blog/tests/test_modelmixintestcase.py
+++ mysite/blog/tests/test_modelmixintestcase.py
@@ -5,20 +5,20 @@ from django.contrib.auth.models import User
 
 class ModelMixinTestCase(TestCase):
     def setUp(self):
-        self.test_user=User.objects.create_user(
+        self.user=User.objects.create_user(
             username= "kaaviya",
             password="123",
             )   
 
-        self.test_post_object_draft= Post.objects.create(
+        self.draft_post= Post.objects.create(
             title='description',
-            author=self.test_user,
+            author=self.user,
             body='hi i am kaaviya',
             status='draft'
         )
-        self.test_post_object_published= Post.objects.create(
+        self.published_post= Post.objects.create(
             title='testing title',
-            author=self.test_user,
+            author=self.user,
             body='hi i am Madhu',
             status='published'
         )
