d69f84e220ed394c71739aa69cc5f615b9dd72f4
kaaviya98 <kaaviya@testpress.in>
2022-11-16 11:02:03 +0530

    Refractor : TestCases

--- mysite/blog/tests/test_models.py
+++ mysite/blog/tests/test_models.py
@@ -1,25 +1,19 @@
 from blog.tests.test_ModelMixinTestCase import ModelMixinTestCase
 from django.test import TestCase
+from django.urls import reverse, resolve
 
 
-class Test_model_method(ModelMixinTestCase,TestCase):
-
+class Test_model_method(ModelMixinTestCase, TestCase):
     def test_absolute_url_in_model(self):
-                self.assertEqual(self.post_detail_url, self.published_post.get_absolute_url())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        self.assertEqual(
+            reverse(
+                "blog:post_detail",
+                args=[
+                    self.published_post.publish.year,
+                    self.published_post.publish.month,
+                    self.published_post.publish.day,
+                    self.published_post.slug,
+                ],
+            ),
+            self.published_post.get_absolute_url(),
+        )
