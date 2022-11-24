93df2916fe4c1b678ee6921fae8cf4a10bb014b9
kaaviya98 <113660479+kaaviya98@users.noreply.github.com>
2022-11-24 17:42:58 +0530

    Refactor list view by using generic view (#12)
    * Refract_list_view_by_using_generic_view
    
    * Refactor by generic view
    
    * Refactor: Views and html page
    
    * Refactor: Test case
    
    * Refactor: Method name for testcase

--- mysite/blog/tests/test_urls.py
+++ mysite/blog/tests/test_urls.py
@@ -1,12 +1,14 @@
 from django.test import SimpleTestCase
 from django.urls import resolve, reverse
-from blog.views import post_list, post_detail
+from blog.views import PostListView, post_detail
 from blog.tests.test_modelmixintestcase import ModelMixinTestCase
 
 
 class TestUrls(ModelMixinTestCase, SimpleTestCase):
     def test_post_list_url_is_resolved(self):
-        self.assertEquals(resolve(reverse("blog:post_list")).func, post_list)
+        self.assertEquals(
+            resolve(reverse("blog:post_list")).func.view_class, PostListView
+        )
 
     def test_post_detail_url_is_resolved(self):
         self.assertEquals(
