15bf1ea5a6497acc3d7b82b3383dcab8bec566f2
kaaviya98 <kaaviya@testpress.in>
2022-11-29 12:54:34 +0530

    Refactor:Reformatting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
