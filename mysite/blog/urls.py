f0301a6ecfef27bc9534a68b17d0ab50b042cded
Kaaviya Elango <113660479+kaaviya98@users.noreply.github.com>
2022-11-30 16:16:05 +0530

    Add support by adding  tag to post (#15)
    * Add_support_by_adding__tag_to_post
    
    * Refactor:Rebase
    
    * Refactor:template
    
    * Add requirements.txt file

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -16,4 +16,9 @@ urlpatterns = [
         name="post_share",
     ),
     path("<int:post_id>/comment/", views.add_comment, name="add_comment"),
+    path(
+        "tag/<slug:tag_slug>/",
+        views.PostListByTagview.as_view(),
+        name="post_list_by_tag",
+    ),
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "taggit",
     "blog.apps.BlogConfig",
     "django.contrib.admin",
     "django.contrib.auth",
